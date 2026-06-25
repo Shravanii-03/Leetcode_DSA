07f9b11d7af8ea71728aa56e94834ec85ec90c7d
Shravani <shravaniisukapalli03@gmail.com>
2026-06-25 22:39:07 +0530

    added leetcode 142

---
+++ linked lists/142_linked_list_cycle_2.cpp
@@ -0,0 +1,61 @@
+#brute
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+    unordered_set<ListNode*> visited;
+    ListNode* temp=head;
+
+    while(temp!=nullptr){
+        if(visited.find(temp)!=visited.end()){
+            return temp;
+        }
+
+    visited.insert(temp);
+    temp=temp->next;
+    }
+    return nullptr;
+
+    }
+};
+
+
+#optimal 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+    
+    ListNode* slow=head;
+    ListNode* fast=head;
+
+    while(fast!=nullptr && fast->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next->next;
+
+        if(slow==fast){
+            slow=head;
+            while(slow!=fast){
+                slow=slow->next;
+                fast=fast->next;
+            }
+            return slow;
+        }
+    }
+    return nullptr;
+    }
+};
