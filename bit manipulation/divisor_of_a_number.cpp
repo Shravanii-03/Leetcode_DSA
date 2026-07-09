77c4f7050aa9cb915a71827d5e3cc5f5df9bd267
Shravani <shravaniisukapalli03@gmail.com>
2026-07-09 22:53:17 +0530

    added find divisor

---
+++ bit manipulation/divisor_of_a_number.cpp
@@ -0,0 +1,15 @@
+class solution{
+public:
+vector<int>getdiv(int n){
+vector<int>res;
+for(int i=1;i*i<=n;i++){
+if(n%i==0){
+res.push_back(i);
+}
+if(i!=n/i){
+res.push_back(n/i);
+}
+}
+return res;
+}
+};
