e2e46f9bf18b69f681e22a1a9b263c7b17cb1907
Shravani <shravaniisukapalli03@gmail.com>
2026-07-15 17:24:33 +0530

    added prefix to infix

---
+++ stacks/prefix_infix_conversion.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string prefixToInfix(string s) {
+
+        stack<string> st;
+
+        for(int i = s.size() - 1; i >= 0; i--)
+        {
+            char c = s[i];
+
+            if((c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c >= '0' && c <= '9'))
+            {
+                st.push(string(1, c));
+            }
+            else
+            {
+                string op1 = st.top();
+                st.pop();
+
+                string op2 = st.top();
+                st.pop();
+
+                string temp = "(" + op1 + c + op2 + ")";
+
+                st.push(temp);
+            }
+        }
+
+        return st.top();
+    }
+};
