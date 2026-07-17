0e39f625a76aa9717cf70cab84a9560615c3ae53
Shravani <shravaniisukapalli03@gmail.com>
2026-07-17 21:11:54 +0530

    added next greater

---
+++ stacks/next_greater_element.cpp
@@ -0,0 +1,41 @@
+#brute
+class Solution {
+public:
+    vector<int> nextLargerElement(vector<int> arr) {
+        int n = arr.size();
+        vector<int> ans(n, -1);
+	for(int i=0;i<n;i++){
+	for(int j=i+1;i<n;j++){
+	if(arr[j]>arr[i]){
+	ans[i]=arr[j];
+	break;
+}
+
+}
+}
+
+return ans;
+}
+};
+
+
+#optimal
+class Solution {
+public:
+
+    vector<int> nextLargerElement(vector<int> arr) {
+	int n = arr.size(); 
+        vector<int> ans(n,-1);
+        stack<int> st;
+
+for(int i=n-1;i>=0;i--){
+	while(!st.empty() && st.top()<=arr[i]){
+	st.pop();
+	}
+	if(!st.empty()){
+	ans[i]=	st.top();
+}
+st.push(arr[i]);
+}
+return ans;
+}};
