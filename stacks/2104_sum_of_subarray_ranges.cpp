358c22dd212e12282f675a0c95730acc069516e7
Shravani <shravaniisukapalli03@gmail.com>
2026-07-18 21:30:14 +0530

    added 2104

---
+++ stacks/2104_sum_of_subarray_ranges.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int mini=nums[i];
+            int maxi=nums[i];
+            for(int j=i;j<n;j++){
+                mini=min(mini, nums[j]);
+                maxi=max(maxi, nums[j]);
+                long long range=(long long)maxi-mini;
+                sum=sum+range;
+            }
+        }
+        return sum;
+    }
+};
