0532b273ca9c9232400a758a59a478a7f31fd88c
Shravani <shravaniisukapalli03@gmail.com>
2026-08-01 10:26:13 +0530

    added 2500

---
+++ heaps/2500_delete_greatest_value_in_each_row.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+    int n=grid.size();
+    int m=grid[0].size();
+
+    for(int i=0;i<n;i++){
+        sort(grid[i].begin(), grid[i].end());
+    }
+    int ans=0;
+    for(int j=0;j<m;j++){
+        int maxi=0;
+        for(int i=0;i<n;i++){
+           maxi=max(maxi,grid[i][j]);
+        }
+    ans+=maxi;
+    }
+    return ans;
+    }
+};
