270c6be66655ce9f17aede3d67b27ec3d1ff460c
Shravani <shravaniisukapalli03@gmail.com>
2026-08-05 23:42:06 +0530

    added leetcode 108

---
+++ trees/108_convert_sorted_array_to_binary_search_tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* convert(vector<int>nums, int l, int r){
+        if(l>r) return nullptr;
+        int mid=(l+r)/2;
+        TreeNode* root=new TreeNode(nums[mid]);
+        root->left=convert(nums, l, mid-1);
+        root->right=convert(nums, mid+1, r);
+        return root;
+
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return convert(nums, 0, nums.size()-1);
+    }
+};
